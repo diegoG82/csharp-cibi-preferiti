d330d78eb378d9d816e82908bb32608feb341282
diegoG82 <diegogiardina@hotmail.it>
2023-09-05 14:16:06 +0200

    Aggiunta lunghezza lista cibi

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -1,4 +1,9 @@
 ﻿//Creazione Array cibi preferiti
 string[] CibiPreferiti = { "Pizza", "Hamburger", "Mascarpone", "Cioccolato", "Sushi" };
 
+//La lunghezza della classifica dei cibi
+Console.WriteLine("la lunghezza della classifica è: " + CibiPreferiti.Length + " Alimenti");
+
+
+
 
