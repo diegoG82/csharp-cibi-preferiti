302aad76e16e007e8d5a1544c7f1c449081482ee
diegoG82 <diegogiardina@hotmail.it>
2023-09-05 16:40:32 +0200

    Sistemazione scrittura codice

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -1,8 +1,8 @@
 ﻿//Creazione Array cibi preferiti
-string[] CibiPreferiti = { "Pizza", "Hamburger", "Mascarpone", "Patatine",  "Cioccolato", "Sushi" };
+string[] CibiPreferiti = { "Pizza", "Hamburger", "Mascarpone", "Patatine", "Cioccolato", "Sushi" };
 
 //La lunghezza della classifica dei cibi
-Console.WriteLine("la lunghezza della classifica dei miei cibi preferiti è di : " + CibiPreferiti.Length + " Alimenti");
+Console.WriteLine($"la lunghezza della classifica dei miei cibi preferiti è di: { CibiPreferiti.Length} Alimenti");
 
 //Riga vuota per spaziare
 Console.WriteLine();
@@ -11,20 +11,21 @@ Console.WriteLine();
 Console.WriteLine("La mia personale classifica dei cibi:");
 for (int i = 0; i < CibiPreferiti.Length; i++)
 {
-    Console.WriteLine(i + 1  + "." +  CibiPreferiti[i]);
+    Console.WriteLine($"{i + 1} {CibiPreferiti[i]}");
 }
 
 //Riga vuota per spaziare
 Console.WriteLine();
 
+
 //Il vostro cibo top (prima posizione della classifica)
-Console.WriteLine("il mio cibo preferito e' la " + CibiPreferiti[0]);
+Console.WriteLine($"Il mio cibo preferito e' la {CibiPreferiti[0]}" );
 
 //Riga vuota per spaziare
 Console.WriteLine();
 
 //Il vostro cibo preferito ma non troppo (ultima posizione della classifica)
-Console.WriteLine("il mio cibo preferito ma non troppo è il " + CibiPreferiti[CibiPreferiti.Length - 1]);
+Console.WriteLine($"il mio cibo preferito ma non troppo è il {CibiPreferiti[CibiPreferiti.Length - 1]} "  );
 
 //Riga vuota per spaziare
 Console.WriteLine();
@@ -40,16 +41,14 @@ if (CibiPreferiti.Length % 2 == 0)
 
     {
     //Casistica array lungo pari   
-    int cibo1 = (CibiPreferiti.Length / 2);
-    int cibo2 = cibo1 + 1;
-    Console.WriteLine("i miei cibi di meta classifica sono: " + CibiPreferiti[cibo1] + " e " + CibiPreferiti[cibo2]);
+    var cibo1 = (CibiPreferiti.Length / 2 - 1);
+    var cibo2 = cibo1 + 1;
+    Console.WriteLine($"i miei cibi di meta classifica sono: {CibiPreferiti[cibo1]} e {CibiPreferiti[cibo2]}  ");
 }
 else
 {
     //Casistica array lungo dispari
-    int cibo = CibiPreferiti.Length / 2;
-    Console.WriteLine("il mio cibo di meta classifica è il : " + CibiPreferiti[cibo]);
-
-
+    var cibo = CibiPreferiti.Length / 2;
+    Console.WriteLine($"il mio cibo di meta classifica è il: {CibiPreferiti[cibo]}" );
 }
 
